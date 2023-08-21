ecb2bae903b992b6ef1ab41768622b931e28faaf
Barbara Fajardo <barbarafajardo92@gmail.com>
2023-08-21 12:46:32 +0100

    amend routes in suggRouter.js file

--- router/suggRouter.js
+++ router/suggRouter.js
@@ -4,9 +4,9 @@ const router = express.Router()
 const suggestionsController = require('../controller/suggestions')
 
 router.get('/', suggestionsController.index)
-router.get('/:category', suggestionsController.show)
-router.get('/:id', suggestionsController.showById)
-router.post('/', suggestionsController.create)
+router.get('/:category', suggestionsController.showCategory)
+router.get('/:id', suggestionsController.showId)
+router.post('/:category', suggestionsController.create)
 router.patch('/:id', suggestionsController.update)
 router.delete('/:id', suggestionsController.destroy)
 
