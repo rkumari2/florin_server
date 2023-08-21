50fdcea5dcb9366e279400dca434c05d6523cb8c
“rkumari2” <rubinakumari@mail.com>
2023-08-21 10:06:19 +0100

    add getAll function to model

---
+++ model/Suggestion.js
@@ -0,0 +1,22 @@
+const db = require('../database/connect')
+
+
+class Suggestion {
+    constructor(data) {
+        this.id = data.id
+        this.category_id = data.category_id
+        this.title = data.title
+        this.content = data.content
+        this.user_id = data.user_id
+    }
+
+    static async getAll () {
+        const response = await db.query('SELECT * FROM suggestions;')
+        if (response.rows.length === 0) {
+            throw new Error ('No suggestions available')
+        } else {
+            console.log(response)
+            return response.rows.map(s => new Suggestion(s))
+        }
+    }
+}
