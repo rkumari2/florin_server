54210ba247a359bffa5628617ed9f8038b434862
Barbara Fajardo <barbarafajardo92@gmail.com>
2023-08-22 10:24:36 +0100

    add :id/suggestion

--- router/categoriesRouter.js
+++ router/categoriesRouter.js
@@ -6,6 +6,7 @@ const categoriesController = require('../controller/categories')
 //cateogry/:id/suggestion
 categoryRouter.get('/', categoriesController.index) //not working as we are expected to pass a string but in the models we are expecting an ID
 categoryRouter.get('/:id', categoriesController.showId)
+categoryRouter.get('/:id/suggestion', categoriesController.findBySuggestion)
 // router.post('/:category', suggestionsController.create) // if issue above not resolved, not functioning 
 
 module.exports = categoryRouter
