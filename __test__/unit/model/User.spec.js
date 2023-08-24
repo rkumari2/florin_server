ea372c52d8b3fb7ca1a801b7efe9d28e8b6af630
“rkumari2” <rubinakumari@mail.com>
2023-08-24 16:57:07 +0100

    test for getById for User class

--- __test__/unit/model/User.spec.js
+++ __test__/unit/model/User.spec.js
@@ -29,4 +29,28 @@ describe('User model', () => {
             expect(result).toHaveLength(2)
         })
     })
+
+    describe('getById', () => {
+        it('returns a user based on their id', async() => {
+            let data = {id: 1, username: 'user1', password: 'jkl'}
+            jest.spyOn(db, 'query').mockResolvedValueOnce({rows: [data]})
+
+            const result = await User.getById(1)
+            expect(result).toBeInstanceOf(User)
+            expect(result.id).toBe(1)
+            expect(result.username).toBe('user1')
+        })
+
+        it.only('returns an error on db query error', async () => {
+            jest.spyOn(db, 'query').mockRejectedValue(new Error('unable to locate user'))
+
+            try {
+                await User.getById(1)
+            } catch (error) {
+                expect(error).toBeTruthy()
+                expect(error).toBeDefined()
+                expect(error.message).toBe('unable to locate user')                
+            }
+        })
+    })
 })
